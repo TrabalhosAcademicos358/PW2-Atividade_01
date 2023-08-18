2e6e2cfac0dbf052df6f040ba7a68d8309aad3b4
Pedro <pedrojuniorifpb@gmail.com>
2023-08-18 16:44:31 -0300

    Começando a estilizar o card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import img_01 from './img/img_01.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main className="card">
+        <header className='header'>
+          <div className='header-rectagle' />
+        </header>
+        
+        <span>
+          <h1 className='title'>Encontre <span className='text-contrast'>Fábio</span> no local de partida</h1>
+          <h2 className='subTitle'>Chega em 3 minutos (800 metros)</h2>
+        </span>
+
+        <div className='section-analysis'>
+          <img className='img' src={img_01} alt='Foto do usuário com carro'/>
+          <span>
+            <h1 className='title'>BCD0D19</h1>
+            <h2 className='subTitle'>Honda Civic Roxo</h2>
+          </span>
+        </div>
+      </main>
     </div>
   );
 }
